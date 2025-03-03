bc0e68afd7ad4d04b07cc2e21eca4edceede3c74
Ganesh Dutt <gdutt2304@gmail.com>
2025-03-03 22:23:22 +0530

    RenameFile

--- Day1/fileSystem/code3.js
+++ Day1/fileSystem/code3.js
@@ -5,3 +5,13 @@ fs.appendFile("./Day1/Files/example.txt", "This is appended data.", (err) => {
   if (err) throw err;
   console.log("Data appended successfully!");
 });
+
+// rename file
+fs.rename(
+  "./Day1/Files/example.txt",
+  "./Day1/Files/RenameExample.txt",
+  (err) => {
+    if (err) throw err;
+    console.log("File renamed successfully!");
+  }
+);
